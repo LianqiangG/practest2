55db4595485103815320a6d3e2c4167bd83acabf
lianqiangG <guanlianqiang@163.com>
2023-09-10 22:16:52 +0930

    1st round

--- Ticketing/Ticketing/TicketPrice.cs
+++ Ticketing/Ticketing/TicketPrice.cs
@@ -17,7 +17,7 @@ namespace Ticketing
         const decimal mdecBalcony = 35.5m;
         const decimal mdecGeneral = 28.75m;
         const decimal mdecBox = 62.0m;
-        const decimal mdecDiscount = 5.0m; 
+        const decimal mdecDiscount = 6.0m; //change the ticket price 1st round
 
         private int Section
         {
